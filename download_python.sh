1965e5b68505f122d9f4358095177e0858eed49b
Zachary Blackwood <zachary@streamlit.io>
2022-12-14 11:49:04 -0500

    Update

--- download_python.sh
+++ download_python.sh
@@ -3,23 +3,13 @@ release_date="20221106"
 filename="cpython-3.8.15+20221106-aarch64-apple-darwin-install_only.tar.gz"
 
 standalone_python="python/"
-destination_folder="$HOME/.streamlit-desktop/envs/default/"
 
-# If the destination folder doesn't exist, download the standalone Python
-
-if [ ! -d "$destination_folder" ]; then
+if [ ! -d "$standalone_python" ]; then
     wget https://github.com/indygreg/python-build-standalone/releases/download/${release_date}/${filename}
     tar -xzvf ${filename}
     rm -rf ${filename}
     # Now delete the test/ folder, saving about 23MB of disk space
     rm -rf python/lib/python3.8/test
-
-    mkdir -p ${destination_folder}
-
-    # For now, just copy the python folder to the default Streamlit Desktop environment
-    cp -r python/* ${destination_folder}
-
-    # rm -rf python/
 else
     echo "Python already exists in the default Streamlit Desktop environment"
 fi
