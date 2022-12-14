9df4fdc8b30f4c584202b2cdb38baf3dd7708298
Tyler Simons <tjsimons@gmail.com>
2022-12-14 13:44:14 -0800

    Collapsible logs

--- renderer.js
+++ renderer.js
@@ -105,3 +105,19 @@ st.write(f"{num}^{num}: ", num**num)
 
     updated(contents);
 })
+
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
