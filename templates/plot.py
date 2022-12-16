1bdf520e181834c7c37b9fdc0d80c3387457f8bd
Tyler Simons <tjsimons@gmail.com>
2022-12-15 22:02:24 -0800

    Update the apps for examples

--- templates/plot.py
+++ templates/plot.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Let's see some :bar_chart:")
-st.write("Some charts")
+st.title(":bar_chart: Show me your moves! ")
+st.write("Graphical, my dear watson :male-detective:")
 
-df = pd.DataFrame({"a": [1, 2, 3], "b": [2, 5, 10]})
-st.line_chart(df)
+df = pd.DataFrame({"a": [1, 2, 3], "b": [2, 5, 10], "c": [3, 2, 1]})
+
+t1, t2, t3 = st.tabs(["Lines", "Bars", "Areas"])
+with t1:
+    st.subheader("I'm a-line! :chart_with_upwards_trend:")
+    st.line_chart(df)
+with t2:
+    st.subheader("Meet me at the bar! :bar_chart:")
+    st.bar_chart(df)
+with t3:
+    st.subheader("Area-nah Grande :microphone:")
+    st.area_chart(df)
 st.write("# :boom: :boom: :boom: :boom: :boom:")
-st.bar_chart(df)
